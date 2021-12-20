c3aae477adae708babe09fe90f49055944e7c6a7
tomast1337 <vycasnicolas@gmail.com>
2021-12-20 04:31:15 -0300

    Initial p5 set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import p5 from 'p5';
+import style from'./App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.myRef = React.createRef()
+        this.state = {
+            wight: 0,
+            height: 0,
+        }
+    }
+
+    Sketch = (p5) => {
+        let xoff = 0;
+        let yoff = 0;
+        let size = 10;
+        let r = 255;
+        let g = 255;
+        let b = 255;
+        p5.setup = () => {
+            p5.createCanvas(630, 400);
+            p5.background(0);
+
+            xoff = p5.width / 2;
+            yoff = p5.height / 2;
+        };
+
+        p5.draw = () => {
+            p5.fill(r, g, b);
+            p5.stroke(0);
+            p5.strokeWeight(1);
+            
+            xoff += 0.01;
+            yoff += 0.01;
+
+            let x = p5.noise(xoff*2) * p5.width;
+            let y = p5.noise(yoff*2) * p5.height;
+            size = p5.noise(xoff) * 80;
+            p5.ellipse(x, y, size, size);
+            
+            r = p5.noise(xoff) * 255;
+            g = p5.noise(yoff) * 255;
+            b = p5.noise(xoff,yoff) * 255;
+
+        };
+    }
+    componentDidMount() {
+        this.myP5 = new p5(this.Sketch, this.myRef.current)
+    }
+    render() {
+        return (
+            <>
+                <div className='title'>
+                    <h1>Black Metal Logo Generator</h1>
+                    <h3>by Nicolas Vycas Nery</h3>
+                </div>
+                <div className='canvasContainer' ref={this.myRef}>
+
+                </div>
+                <div>
+                    <button onClick={() => this.myP5.background(0)}>Clear</button>
+                </div>
+            </>
+        )
+    }
 }
 
 export default App;
